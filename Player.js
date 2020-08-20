02ece97a5a55dfa1b010d99e811f12133fc5efb6
unknown <mahyar.tajeri@gmail.com>
2020-08-20 16:25:32 -0400

    player file

---
+++ Player.js
@@ -0,0 +1,9 @@
+class Player{
+	let size = 30;
+
+	constructor(type){
+		this.type = type; //AI or real player
+	}	
+
+	
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+let player;
+
 function setup(){
-  createCanvas(600, 600);
+	player = new Player(0);
+	createCanvas(600, 600);
 }
 
 function draw(){
-  background(220);
-  fill(255, 0, 0);
-  ellipse(width/2, height/2, 20, 20);
+	
+}
+
+function drawPlayer(){
+	ellipse(0, 0, player.size, player.size);
 }
