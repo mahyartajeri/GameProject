d7287f3547ee2ff987841488825e50f56cdcb191
unknown <mahyar.tajeri@gmail.com>
2020-08-20 17:49:37 -0400

    added moving circle with acceleration

--- Player.js
+++ Player.js
@@ -1,9 +1,27 @@
 class Player{
-	let size = 30;
 
-	constructor(type){
+
+	constructor(type, x, y){
 		this.type = type; //AI or real player
+		this.x = x;
+		this.y = y;
+		this.size = 30;
+		this.x_vel = 0;
+		this.y_vel = 0;
 	}	
 
-	
+	drawPlayer(){
+		this.x = this.x + this.x_vel;
+		this.y = this.y + this.y_vel;
+		if(this.type == 0){
+			push();
+			stroke(3);
+			fill(255, 0, 0);
+			ellipse(this.x, this.y, this.size, this.size);
+			pop();
+		}
+	}
+
+
+
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,40 @@
 let player;
+let acc = 0.5;
 
 function setup(){
-	player = new Player(0);
-	createCanvas(600, 600);
-}
-
-function draw(){
+	
+	let canvas = createCanvas(600, 600);
+	player = new Player(0, width/2, height/2);
+	canvas.position(300, 100);
+	
 	
 }
 
-function drawPlayer(){
-	ellipse(0, 0, player.size, player.size);
+function draw(){
+	background(220);
+	checkMovement();
+	player.drawPlayer()
+}
+
+function checkMovement(){
+	if(keyIsDown(87)){ //keycode for w
+		if(player.y_vel > -10){
+			player.y_vel -= acc;
+		}
+	}
+	if(keyIsDown(83)){ //keycode for s
+		if(player.y_vel < 10){
+			player.y_vel += acc;
+		}
+	}
+	if(keyIsDown(65)){ // keycode for a
+		if(player.x_vel > -10){
+			player.x_vel -= acc;
+		}
+	}
+	if(keyIsDown(68)){ //keycode for d
+		if(player.x_vel < 10){
+			player.x_vel += acc;
+		}
+	}
 }
