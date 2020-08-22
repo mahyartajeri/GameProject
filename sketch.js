8c3ebafc81d3cfd89d4e727d3697a45513d77fe6
unknown <mahyar.tajeri@gmail.com>
2020-08-22 16:06:43 -0400

    added camera that follows the player

--- sketch.js
+++ sketch.js
@@ -1,20 +1,23 @@
 let player;
-let acc = 0.5;
-let friction = 0.25;
+let acc = 0.5; //acceleration 
+let friction = 0.25; 
+
 function setup(){
-	
 	let canvas = createCanvas(600, 600);
 	player = new Player(0, width/2, height/2);
 	canvas.position(300, 100);
-	
-	
 }
 
 function draw(){
 	background(220);
+	//camera.zoom = 0.5;
+	rect(100, 100, 50, 50);
 	checkForMovement();
 	slowDown();
 	player.drawPlayer()
+	camera.position.x = player.x;
+	camera.position.y = player.y;
+
 }
 
 function checkForMovement(){
